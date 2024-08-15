73d5972820f1bc35bf9a062c795bcff3f2655caf
ErsHoeghet <112584470+ErsHoeghet@users.noreply.github.com>
2024-08-15 14:45:18 +0200

    Added seed.ts and added som stuff so that a user can tryout the project with a "gues account".

---
+++ backend/scripts/products.ts
@@ -0,0 +1,170 @@
+export const products = [
+    {
+      productName: 'Burk med stenar',
+      description: 'Väldigt vackra stenar från Österlens pittoreska vidder.',
+      price: 192,
+      imgPath: '../../frontend/src/assets/IMG/burkmedstenar',
+      category: 'Vuxna',
+      stockQty: 13
+    },
+    {
+      productName: 'Leksakshus',
+      description: 'Något slags pappershus. Jättekul att leka med!',
+      price: 10,
+      imgPath: '/assets/IMG/',
+      category: 'Barn',
+      stockQty: 233
+    },
+    {
+      productName: 'Leksaksbil',
+      description: 'En vacker bil som alla barn gillar.',
+      price: 210,
+      imgPath: '/assets/IMG/',
+      category: 'Barn',
+      stockQty: 52
+    },
+    {
+      productName: 'Spidermankeps',
+      description: 'En keps för barn. Eller vuxen med litet huvud.',
+      price: 165,
+      imgPath: '/assets/IMG/',
+      category: 'Barn',
+      stockQty: 14
+    },
+    {
+      productName: 'Mugg',
+      description: 'Otroligt välgjord mugg.',
+      price: 325,
+      imgPath: '/assets/IMG/',
+      category: 'Vuxna',
+      stockQty: 3
+    },
+    {
+      productName: 'Teckning',
+      description: 'Rosa teckning.',
+      price: 500,
+      imgPath: '/assets/IMG/',
+      category: '',
+      stockQty: 1
+    },
+    {
+      productName: 'Teckning',
+      description: 'Pappa.',
+      price: 500,
+      imgPath: '/assets/IMG/',
+      category: '',
+      stockQty: 1
+    },
+    {
+      productName: 'Snus - Kaliber Salmiak',
+      description: 'Billigt men torrt sattyg. Har vi slut på alla andra produkter kan du köpa detta. Till och med Vårgårda är bättre.',
+      price: 30,
+      imgPath: '/assets/IMG/',
+      category: 'Vuxna',
+      stockQty: 300
+    },
+    {
+      productName: 'Vattenkanna',
+      description: 'Modell mindre. Här på butiken uppskattar vi att den kan hålla ca 2,5 deciliter vatten. Fin som prydnad i det rustika hemmet.',
+      price: 270,
+      imgPath: '/assets/IMG/',
+      category: '',
+      stockQty: 20
+    },
+    {
+      productName: 'Docka',
+      description: 'Söt docka.',
+      price: 130,
+      imgPath: '/assets/IMG/',
+      category: 'Barn',
+      stockQty: 666
+    },
+    {
+      productName: 'Gitarr',
+      description: 'Dess spröda ton är ej bisarr - men det finns glädje i min gitarr.',
+      price: 3200,
+      imgPath: '/assets/IMG/',
+      category: 'Vuxna',
+      stockQty: 10
+    },
+    {
+      productName: 'Neil Young - Fredsförklaring',
+      description: 'Bok författad av den fantastiske artisten Neil Young.',
+      price: 420,
+      imgPath: '/assets/IMG/',
+      category: 'Vuxna',
+      stockQty: 21
+    },
+    {
+      productName: 'Raspberry Pi',
+      description: 'Endast kartong.',
+      price: 15,
+      imgPath: '/assets/IMG/',
+      category: 'Vuxna',
+      stockQty: 70
+    },
+    {
+      productName: 'Snus - Vårgårda Bär',
+      description: 'Inget märkvärdigt.',
+      price: 46,
+      imgPath: '/assets/IMG/',
+      category: 'Vuxna',
+      stockQty: 500
+    },
+    {
+      productName: 'Tutti Frutti',
+      description: 'Tidlöst godis som tuggats av generation efter generation.',
+      price: 15,
+      imgPath: '/assets/IMG/',
+      category: '',
+      stockQty: 143
+    },
+    {
+      productName: 'Konstnärligt kodlås',
+      description: 'Kodlås designat efter dito i datorspelet RUST.',
+      price: 150,
+      imgPath: '/assets/IMG/',
+      category: '',
+      stockQty: 0
+    },
+    {
+      productName: 'Munspel - Lee Oskar',
+      description: 'Äkta kvalitet.',
+      price: 250,
+      imgPath: '/assets/IMG/',
+      category: 'Vuxna',
+      stockQty: 9
+    },
+    {
+      productName: 'Burk med pengar',
+      description: 'Burk med pengar...',
+      price: 200,
+      imgPath: '/assets/IMG/',
+      category: 'Vuxna',
+      stockQty: 1
+    },
+    {
+      productName: 'Såg',
+      description: 'Verktyg för en äkta karl.',
+      price: 260,
+      imgPath: '/assets/IMG/',
+      category: 'Vuxna',
+      stockQty: 14
+    },
+    {
+      productName: 'Liten skål',
+      description: 'Liten och nätt.',
+      price: 25,
+      imgPath: '/assets/IMG/',
+      category: 'Vuxna',
+      stockQty: 4
+    },
+    {
+      productName: 'Strumpor',
+      description: 'Barnstrumpor av högsta kvalitet!',
+      price: 25,
+      imgPath: '/assets/IMG/',
+      category: 'Barn',
+      stockQty: 77
+    },
+  ];
